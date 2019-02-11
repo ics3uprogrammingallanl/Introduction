9c50ed9411c30aa015a966295219af121091cde6
AL10120 <allan.leveille@ocsbstudent.ca>
2019-02-11 12:24:15 -0500

    Added perimeter, Area, Volume by Allan L

--- HelloWorldIntAllanL/HelloWorldIntAllanL/HelloWorldIntForm.Designer.cs
+++ HelloWorldIntAllanL/HelloWorldIntAllanL/HelloWorldIntForm.Designer.cs
@@ -28,20 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.radEnglish = new System.Windows.Forms.RadioButton();
+            this.radFrench = new System.Windows.Forms.RadioButton();
+            this.radArabic = new System.Windows.Forms.RadioButton();
+            this.radGreek = new System.Windows.Forms.RadioButton();
+            this.radSwedish = new System.Windows.Forms.RadioButton();
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.grbHelloWorldInt = new System.Windows.Forms.GroupBox();
+            this.grbHelloWorldInt.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // radEnglish
+            // 
+            this.radEnglish.AutoSize = true;
+            this.radEnglish.Location = new System.Drawing.Point(6, 45);
+            this.radEnglish.Name = "radEnglish";
+            this.radEnglish.Size = new System.Drawing.Size(79, 24);
+            this.radEnglish.TabIndex = 0;
+            this.radEnglish.TabStop = true;
+            this.radEnglish.Text = "English";
+            this.radEnglish.UseVisualStyleBackColor = true;
+            this.radEnglish.CheckedChanged += new System.EventHandler(this.radEnglish_CheckedChanged);
+            // 
+            // radFrench
+            // 
+            this.radFrench.AutoSize = true;
+            this.radFrench.Location = new System.Drawing.Point(97, 45);
+            this.radFrench.Name = "radFrench";
+            this.radFrench.Size = new System.Drawing.Size(77, 24);
+            this.radFrench.TabIndex = 1;
+            this.radFrench.TabStop = true;
+            this.radFrench.Text = "French";
+            this.radFrench.UseVisualStyleBackColor = true;
+            this.radFrench.CheckedChanged += new System.EventHandler(this.radFrench_CheckedChanged);
+            // 
+            // radArabic
+            // 
+            this.radArabic.AutoSize = true;
+            this.radArabic.Location = new System.Drawing.Point(188, 45);
+            this.radArabic.Name = "radArabic";
+            this.radArabic.Size = new System.Drawing.Size(72, 24);
+            this.radArabic.TabIndex = 2;
+            this.radArabic.TabStop = true;
+            this.radArabic.Text = "Arabic";
+            this.radArabic.UseVisualStyleBackColor = true;
+            this.radArabic.CheckedChanged += new System.EventHandler(this.radArabic_CheckedChanged);
+            // 
+            // radGreek
+            // 
+            this.radGreek.AutoSize = true;
+            this.radGreek.Location = new System.Drawing.Point(279, 45);
+            this.radGreek.Name = "radGreek";
+            this.radGreek.Size = new System.Drawing.Size(71, 24);
+            this.radGreek.TabIndex = 3;
+            this.radGreek.TabStop = true;
+            this.radGreek.Text = "Greek";
+            this.radGreek.UseVisualStyleBackColor = true;
+            this.radGreek.CheckedChanged += new System.EventHandler(this.radGreek_CheckedChanged);
+            // 
+            // radSwedish
+            // 
+            this.radSwedish.AutoSize = true;
+            this.radSwedish.Location = new System.Drawing.Point(370, 45);
+            this.radSwedish.Name = "radSwedish";
+            this.radSwedish.Size = new System.Drawing.Size(87, 24);
+            this.radSwedish.TabIndex = 4;
+            this.radSwedish.TabStop = true;
+            this.radSwedish.Text = "Swedish";
+            this.radSwedish.UseVisualStyleBackColor = true;
+            this.radSwedish.CheckedChanged += new System.EventHandler(this.radSwedish_CheckedChanged);
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.Font = new System.Drawing.Font("Malgun Gothic", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHelloWorld.Location = new System.Drawing.Point(542, 76);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(425, 86);
+            this.lblHelloWorld.TabIndex = 5;
+            this.lblHelloWorld.Text = "Hello, World";
+            this.lblHelloWorld.Click += new System.EventHandler(this.lblHelloWorld_Click);
+            // 
+            // grbHelloWorldInt
+            // 
+            this.grbHelloWorldInt.Controls.Add(this.radArabic);
+            this.grbHelloWorldInt.Controls.Add(this.radGreek);
+            this.grbHelloWorldInt.Controls.Add(this.radSwedish);
+            this.grbHelloWorldInt.Controls.Add(this.radFrench);
+            this.grbHelloWorldInt.Controls.Add(this.radEnglish);
+            this.grbHelloWorldInt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grbHelloWorldInt.Location = new System.Drawing.Point(583, 284);
+            this.grbHelloWorldInt.Name = "grbHelloWorldInt";
+            this.grbHelloWorldInt.Size = new System.Drawing.Size(458, 100);
+            this.grbHelloWorldInt.TabIndex = 6;
+            this.grbHelloWorldInt.TabStop = false;
+            this.grbHelloWorldInt.Text = "Languages";
             // 
             // frmHelloWorldInt
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.BackgroundImage = global::HelloWorldIntAllanL.Properties.Resources._1_U_R58ahr5dtAvtSLGK2wXg;
+            this.ClientSize = new System.Drawing.Size(1508, 654);
+            this.Controls.Add(this.grbHelloWorldInt);
+            this.Controls.Add(this.lblHelloWorld);
             this.Name = "frmHelloWorldInt";
             this.Text = "Hello World International Allan L";
+            this.Load += new System.EventHandler(this.frmHelloWorldInt_Load);
+            this.grbHelloWorldInt.ResumeLayout(false);
+            this.grbHelloWorldInt.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton radEnglish;
+        private System.Windows.Forms.RadioButton radFrench;
+        private System.Windows.Forms.RadioButton radArabic;
+        private System.Windows.Forms.RadioButton radGreek;
+        private System.Windows.Forms.RadioButton radSwedish;
+        private System.Windows.Forms.Label lblHelloWorld;
+        private System.Windows.Forms.GroupBox grbHelloWorldInt;
     }
 }
 
--- HelloWorldIntAllanL/HelloWorldIntAllanL/HelloWorldIntForm.cs
+++ HelloWorldIntAllanL/HelloWorldIntAllanL/HelloWorldIntForm.cs
@@ -16,5 +16,40 @@ namespace HelloWorldIntAllanL
         {
             InitializeComponent();
         }
+
+        private void frmHelloWorldInt_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radArabic_CheckedChanged(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "مرحبا بالعالم";
+        }
+
+        private void radEnglish_CheckedChanged(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "Hello World";
+        }
+
+        private void radFrench_CheckedChanged(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "Bonjour le monde";
+        }
+
+        private void radGreek_CheckedChanged(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "Γειά σου Κόσμε";
+        }
+
+        private void radSwedish_CheckedChanged(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "Hej världen";
+        }
+
+        private void lblHelloWorld_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
